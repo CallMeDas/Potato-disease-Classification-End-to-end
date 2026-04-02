1c007f0f8a673f8e9fbe36a7e45eefcf9bc23797
callmedas <deepakkdas77@gmail.com>
2026-04-02 15:49:21 +0545

    FastApi model loaded

--- api/main.py
+++ api/main.py
@@ -2,9 +2,13 @@ from fastapi import FastAPI, File, UploadFile
 import numpy as np
 from io import BytesIO
 from PIL import Image
+import tensorflow as tf
 
 
 app = FastAPI()
+
+MODEL = tf.keras.models.load_model('../models/1.keras')
+CLASS_NAMES = ['Early Blight', 'Late Blight', 'Healthy']
 
 @app.get('/ping')
 def ping():
@@ -17,13 +21,21 @@ def read_file_as_image(data) -> np.ndarray:
 
 
 
-@app.get('/predict')
-async def predict():
-    file : UploadFile = File(...)
+@app.post('/predict')
+async def predict(
+    file: UploadFile = File(...)
+):
     image = read_file_as_image(await file.read())
-    return 'ok'
+    image_batch = np.expand_dims(image, 0)
+    prediction = MODEL.predict(image_batch)
+    predicted_class = CLASS_NAMES[np.argmax(prediction[0])]
+    confidence = np.max(prediction[0])
+    return {
+        'Class': predicted_class,
+        'Confidence': float(confidence)
+    }
 
 
 if __name__ == '__main__':
     import uvicorn
-    uvicorn.run(app, host='localhost', port=8000)
+    uvicorn.run(app, host='localhost', port=8001)
